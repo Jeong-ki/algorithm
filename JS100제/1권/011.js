26e5ae53e167d15ead7ac5be67dcb6f5e3c3f24f
Jeong-ki <7863z@naver.com>
2022-02-06 23:07:41 +0900

    js100제 11~15

---
+++ JS100제/1권/011.js
@@ -0,0 +1,9 @@
+// 1부터 100까지 모두 더하는 for문
+
+let s = 0;
+
+for(let i=1; i<=100; i++) {
+  s += i;
+}
+
+console.log(s);
---
+++ JS100제/1권/014.js
@@ -0,0 +1,7 @@
+let 입력 = 2;
+
+if(입력%3 === 0) {
+  console.log("짝");
+} else {
+  console.log(입력);
+}
