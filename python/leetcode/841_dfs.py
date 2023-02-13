ff1b1a22b5018ffc5ea59ad7bc2447e3f22b711f
Jeong-ki <7863z@naver.com>
2023-02-14 00:49:58 +0900

    leetcode 841

--- python/leetcode/841_dfs.py
+++ python/leetcode/841_dfs.py
@@ -1,7 +1,20 @@
 class Solution(object):
     def canVisitAllRooms(self, rooms):
-        arr = [False] * len(rooms)
+        visited = [False] * len(rooms)
+
+        def dfs(room):
+            if visited[room]:
+                return
+
+            visited[room] = True
+            for key in rooms[room]:
+                if not visited[key]:
+                    dfs(key)
+
+        dfs(0)
         
+        return False not in visited
+
 
 solution = Solution()
-print(solution.canVisitAllRooms([[4],[3],[],[2,5,7],[1],[],[8,9],[],[],[6]]))
+print(solution.canVisitAllRooms([[1,3],[3,0,1],[2],[0]]))
